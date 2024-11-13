6cd0378a1cc3adcc2e2d79919e95fddde2698f16
Matt Pinzino <pinzino88@gmail.com>
2024-11-12 20:37:31 -0500

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+        <div>
+          <h1 style={{color: "white"}}>Matt Pinzino</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
